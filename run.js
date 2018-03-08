7452cec96ca77e860255c3990dd579efbac99fb9
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-08 12:19:18 +0000

    added input/output values for test run

--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 var sim = require('./simulator');
-var v = require('./vm');
+var VM = require('./vm');
 
 const INITIAL_POPULATION = 100;
 const GENERATIONS = 1000;
 
 simulator = new sim[0](INITIAL_POPULATION)
-simulator.fitness = (x) => { return (x**2)+4; };
+simulator.fitness((x) => { return (x**2)+4; }, [0,1,2,3,4], [4, 5, 9, 20]);
 simulator.start(GENERATIONS);
