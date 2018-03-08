e847c3e6fcc73a00a1a133fe6b48c50dc926daa6
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-08 12:19:34 +0000

    corrected syntax flaws

--- simulator.js
+++ simulator.js
@@ -1,3 +1,5 @@
+var VM = require('./vm');
+
 function candidate_generator() {
   return [0,0,0,0];
 }
@@ -17,7 +19,7 @@ class Simulator {
 
   fitness(f, input_range=[]) {
     this.inputs = input_range;
-    this.outputs = inputs.map((x) => f.call(x));
+    this.outputs = this.inputs.map((x) => f.call(x));
   }
 
   generation_debug() {
@@ -36,20 +38,21 @@ class Simulator {
 }
 
 class Evaluation {
-  constructor(program, inputs, outputs) {
+  constructor(program=[], inputs=[], outputs=[]) {
     this.program = program;
     this.inputs = inputs;
     this.outputs = outputs;
   }
   run() {
+       let self = this;
        return this.inputs.map(
             function(input, n) { 
-              let vm = new VM;
-              vm.load(this.program);
-              let r = vm.run();
+              let vm = new VM[0];
+              vm.load(self.program);
+              let r = vm.exec();
               let r0 = r[0];
-              Math.abs(this.outputs[n] - r0)
-        }).reduce(0, (i, acc) => acc + i);
+              Math.abs(self.outputs[n] - r0)
+        }).reduce((i, acc) => acc + i, 0);
   }
 }
 
