583d075ff430467469830fa5ce55a607569de997
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-15 14:09:56 +0000

    fixed up VM code ported from ruby, now partially functional but needs a debugger added

--- vm.js
+++ vm.js
@@ -1,53 +1,56 @@
 const DEBUG_MODE = false;
-const REGISTER_SIZE = 2;
+const REGISTER_SIZE = 16;
 
 const MANGLE_UNCHANGED_INPUT = 9000;
-
-const operations = [
-  function(op, r1, r2, r3) {
-    return 0;
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = this.r[r2] + this.r[r3]);
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = this.r[r2] - this.r[r3]);
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = this.r[r2] * this.r[r3]);
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = this.r[r2] / this.r[r3]);
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = this.r[r2] ** this.r[r3]);
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = Math.exp(this.r[r2]));
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = Math.log(this.r[r2]));
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = Math.sqrt(this.r[r2]));
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = Math.sin(this.r[r2]));
-  },
-  function(op, r1, r3, r3) {
-    return (this.r[r1] = Math.cos(this.r[r2]));
-  }
-];
 
 class VM {
   constructor(registers = [], flags = []) {
     this.pc = 0;
     this.set_flags(flags);
     this.initialize_registers(registers);
+    this.map = [
+      (r1, r2, r3) => {
+        return 0;
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = this.r[r2] + this.r[r3]);
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = this.r[r2] - this.r[r3]);
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = this.r[r2] * this.r[r3]);
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = this.r[r2] / this.r[r3]);
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = this.r[r2] ** this.r[r3]);
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = Math.exp(this.r[r2]));
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = Math.log(this.r[r2]));
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = Math.sqrt(this.r[r2]));
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = Math.sin(this.r[r2]));
+      },
+      (r1, r2, r3) => {
+        return (this.r[r1] = Math.cos(this.r[r2]));
+      }
+    ];
   }
 
   initialize_registers(registers) {
-    this.registers = registers;
+    this.r = [];
+    this.r.concat(registers);
+    for (let x = 0; x < REGISTER_SIZE - registers.length; x++) {
+      this.r.push(Math.round(Math.random()*32));
+    }
   }
 
   load(code) {
@@ -67,32 +70,30 @@ class VM {
   }
 
   fetch() {
-    return this.mem[this.pc];
+    return this.mem[this.pc++];
   }
 
   exec() {
-
-    let running = false;
+    let running = true;
 
     while (running == true) {
-
-      let op = this.fetch;
+      let instr = this.fetch();
 
       try {
-        operations[op[0]].call();
+        this.map[instr[0]].call(instr[1], instr[2], instr[3]);
       } catch (e) {
         console.log("Error! NULL!");
         running = false;
+        console.log(e);
       }
 
-      if (this.pc >= this.mem.size) {
+      if (this.pc >= this.mem.length) {
         running = false;
       }
-
     }
 
-    return this.registers;
-
+    console.log(this.r[0]);
+    return this.r;
   }
 }
 
