efe2650349a239e746fa44d9a44315d315ca5bc8
Rehan Jaffer <ray@thelondonvandal.com>
2018-03-16 15:52:31 +0000

    added breed and mutate function, this code is starting to get really messy, add tests and do a refactor

--- simulator.js
+++ simulator.js
@@ -1,11 +1,41 @@
 var VM = require('./vm');
 
-const OPS_NUM = 8;
-const REG_NUM = 15;
-const MAX_CANDIDATE_SIZE = 10;
+const OPS_NUM = 6;
+const REG_NUM = 8;
+const MAX_CANDIDATE_SIZE = 5;
 
 function random_from(start, end) {
-  return Math.round((Math.random() * end) - start);
+  return Math.round((Math.random() * end) + start);
+}
+
+function breed(candidate1, candidate2) {
+
+  let c1_len = candidate1.size;  
+  let c2_len = candidate2.size;  
+
+  let cutpoint1 = random_from(0,c1_len/2);
+  let cutpoint2 = random_from(cutpoint1, c1_len);
+
+  let cutpoint3 = random_from(0, c2_len/2);
+  let cutpoint4 = random_from(cutpoint3, c2_len);
+
+  let segment = candidate2.slice(cutpoint3, cutpoint4);
+  let child = candidate1.slice(0,cutpoint1).concat(segment).concat(candidate1.slice(cutpoint2, c2_len));
+
+  return child;
+
+}
+
+function mutate(candidate) {
+
+  for(let x=0;x<candidate.length;x++) {
+    if (Math.round(Math.random()*10) == 2) {
+      let field = Math.round(Math.random()*4);
+      candidate[x][field] += Math.round(Math.random()*4)-2;
+      candidate[x][field] = Math.abs(candidate[x][field]) % 8;
+    }
+  }
+  return candidate;
 }
 
 function candidate_generator(len) {
@@ -33,7 +63,7 @@ class Simulator {
 
   fitness(f, input_range=[]) {
     this.inputs = input_range;
-    this.outputs = this.inputs.map((x) => f.call(x));
+    this.outputs = this.inputs.map((x) => f(x));
   }
 
   generation_debug() {
@@ -41,13 +71,31 @@ class Simulator {
   }
   start(generations) {
     let self = this;
-    for(let x=0;x<generations;x++) {
-      this.generation.map(
+    for(let g=0;g<generations;g++) {
+      let evaluated = this.generation.map(
           function(c) { 
             let e = new Evaluation(c, self.inputs, self.outputs);
-            let z = e.run();
-            return [c, z[0]];
-      })
+            return [c, e.run()];
+      }).filter((x) => !isNaN(x[1]));
+      let best = evaluated.sort((x,y) => x[1]-y[1]).slice(0,100);
+      if (best[1] == 0) {
+        exit;
+      }
+      let next = best.map((x) => x[0]);
+      let scores = best.map((x) => x[1]);
+      let avg = scores.reduce(((acc,x) => acc+x), 0)/scores.length;
+      let avg_size = next.reduce((acc, x) => acc+x.length, 0)/next.length;
+      console.log(g + "##: Average Score: " + avg + " Average Size: " + avg_size);
+      let children = [];
+
+      for(let x=0;x<next.length-1;x++) {
+        children.push(breed(next[x],next[x+1]));
+      }
+
+      for (let x=0;x<100;x++) {
+        next.push(candidate_generator(random_from(1,100)));
+      }
+      this.generation = next.concat(children).map((x) => mutate(x));
     }
   }
 }
@@ -62,11 +110,11 @@ class Evaluation {
        let self = this;
        return this.inputs.map(
             function(input, n) { 
-              let vm = new VM[0];
+              let vm = new VM[0]([input]);
               vm.load(self.program);
               let r = vm.exec();
               let r0 = r[0];
-              Math.abs(self.outputs[n] - r0)
+              return Math.abs(self.outputs[n] - r0)
         }).reduce((i, acc) => acc + i, 0);
   }
 }
